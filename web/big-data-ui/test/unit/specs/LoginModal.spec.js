28b64af2e5ff4c67391eb58b10c127ac1992f23b
scottynguyen <scottvietphongnguyen@yahoo.de>
2017-11-08 20:27:00 +0100

    Added a simple Test for the Registering process

--- web/big-data-ui/test/unit/specs/LoginModal.spec.js
+++ web/big-data-ui/test/unit/specs/LoginModal.spec.js
@@ -20,5 +20,53 @@ describe('LandingPage.vue', () => {
 
 	});
 
+	// check if the correct register data is transmitted
+	it('should transmit the Input of the register fields', () => {
+		const Constructor = Vue.extend(LoginModal);
+		const registerData = new Constructor().$mount();
+
+		registerData.registerName = "MaxMustermann";
+		registerData.registerEmail = "MaxMustermann@gmail.de";
+		registerData.registerPassword = "CoolPW";
+
+		const button = registerData.$el.querySelector('#registerSubmit');
+		const clickEvent = new window.Event('click');
+		button.dispatchEvent(clickEvent);
+		registerData._watcher.run();
+    	expect(registerData.registerName).to.contain('MaxMustermann');
+    	expect(registerData.registerEmail).to.contain('MaxMustermann@gmail.de');
+    	expect(registerData.registerPassword).to.contain('CoolPW');
+
+	});
+
+/*	// check if the you can register with the same username
+	it('should not be able to register with an existing username', () => {
+		const Constructor = Vue.extend(LoginModal);
+		const registerData = new Constructor().$mount();
+
+		registerData.registerName = "MaxMustermann";
+		registerData.registerEmail = "MaxMustermann@gmail.de";
+		registerData.registerPassword = "CoolPW";
+
+		const button = registerData.$el.querySelector('#registerSubmit');
+		const clickEvent = new window.Event('click');
+		button.dispatchEvent(clickEvent);
+		registerData._watcher.run();
+
+		const Constructor = Vue.extend(LoginModal);
+		const registerData1 = new Constructor().$mount();
+
+		registerData1.registerName = "MaxMustermann";
+		registerData1.registerEmail = "MaxMustermann@gmail.de";
+		registerData1.registerPassword = "CoolPW";
+    	
+    	const button = registerData.$el.querySelector('#registerSubmit');
+		const clickEvent = new window.Event('click');
+		button.dispatchEvent(clickEvent);
+
+		// TODO
+		expect('Some Kind of error!');
+	});
+*/
 
 });
