c7a93a43043dc05cbebfcdb8c468cd650d41356e
Tristan Schneider <schneidtha@gmail.com>
2017-11-17 01:16:49 +0100

    test structure changed. git ignore

--- backendRestDraft/src/test/java/de/pretrendr/controller/AuthenticationControllerTest.java
+++ backendRestDraft/src/test/java/de/pretrendr/controller/AuthenticationControllerTest.java
@@ -1,52 +1,30 @@
-package de.pretrendr;
+package de.pretrendr.controller;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.mock.http.MockHttpOutputMessage;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
 
-import de.pretrendr.boot.Application;
+import de.pretrendr.PretrendrTestBase;
 import de.pretrendr.usermanagement.dataccess.RoleDAO;
 import de.pretrendr.usermanagement.dataccess.UserDAO;
 import de.pretrendr.usermanagement.model.Role;
 import de.pretrendr.usermanagement.model.User;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class)
-@WebAppConfiguration
-public class AuthenticationControllerTest {
-
-	private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
-			MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
-
-	private MockMvc mockMvc;
-
-	private HttpMessageConverter mappingJackson2HttpMessageConverter;
+public class AuthenticationControllerTest extends PretrendrTestBase {
 
 	private User user;
 
@@ -56,24 +34,10 @@ public class AuthenticationControllerTest {
 	private RoleDAO roleDAO;
 
 	@Autowired
-	private WebApplicationContext webApplicationContext;
-
-	@Autowired
 	private UserDAO userDAO;
-
-	@Autowired
-	void setConverters(HttpMessageConverter<?>[] converters) {
-
-		this.mappingJackson2HttpMessageConverter = Arrays.asList(converters).stream()
-				.filter(hmc -> hmc instanceof MappingJackson2HttpMessageConverter).findAny().orElse(null);
-
-		assertNotNull("the JSON message converter must not be null", this.mappingJackson2HttpMessageConverter);
-	}
 
 	@Before
 	public void setup() throws Exception {
-		this.mockMvc = webAppContextSetup(webApplicationContext).build();
-
 		this.roleDAO.deleteAll();
 		this.userDAO.deleteAll();
 
@@ -133,31 +97,8 @@ public class AuthenticationControllerTest {
 				.andExpect(jsonPath("$.password").doesNotExist());
 		// @formatter:on
 	}
-	//
-	// @Test
-	// public void readBookmarks() throws Exception {
-	// mockMvc.perform(get("/" + userName +
-	// "/bookmarks")).andExpect(status().isOk())
-	// .andExpect(content().contentType(contentType)).andExpect(jsonPath("$",
-	// hasSize(2)))
-	// .andExpect(jsonPath("$[0].id", is(this.RolesList.get(0).getId().intValue())))
-	// .andExpect(jsonPath("$[0].uri", is("http://bookmark.com/1/" + userName)))
-	// .andExpect(jsonPath("$[0].description", is("A description")))
-	// .andExpect(jsonPath("$[1].id", is(this.RolesList.get(1).getId().intValue())))
-	// .andExpect(jsonPath("$[1].uri", is("http://bookmark.com/2/" + userName)))
-	// .andExpect(jsonPath("$[1].description", is("A description")));
-	// }
-	//
-	// @Test
-	// public void createBookmark() throws Exception {
-	// String bookmarkJson = json(new Bookmark(this.account, "http://spring.io",
-	// "a bookmark to the best resource for Spring news and information"));
-	//
-	// this.mockMvc.perform(post("/" + userName +
-	// "/bookmarks").contentType(contentType).content(bookmarkJson))
-	// .andExpect(status().isCreated());
-	// }
 
+	@Override
 	protected String json(Object o) throws IOException {
 		MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
 		this.mappingJackson2HttpMessageConverter.write(o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
---
+++ backendRestDraft/src/test/java/de/pretrendr/controller/UserControllerTest.java
@@ -0,0 +1,17 @@
+package de.pretrendr.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.pretrendr.PretrendrTestBase;
+
+public class UserControllerTest extends PretrendrTestBase {
+
+	@Before
+	public void setup() throws Exception {
+	}
+
+	@Test
+	public void test() {
+	}
+}
