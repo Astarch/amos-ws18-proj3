9f5e956097c4bec3d9cfb3298b301f150ea992c6
Florian <florian.muchow@online.de>
2017-11-18 20:55:33 +0100

    last fix

--- architecture/deployment/deploy_prod.sh
+++ architecture/deployment/deploy_prod.sh
@@ -1,11 +1,10 @@
 #!bin/bash
-## Error will make circleci build fail
-set -e
-
-tar -zcvf ~/repo/backendRestDraft.tar.gz backendRestDraft/
+cd ~/repo
+cd ..
+tar -zcvf backendRestDraft.tar.gz repo/
 wget "https://www.dropbox.com/s/4t137xjyadtsgci/AMOS_EC2_KEY.pem"
 echo password | sudo -S chmod 400 AMOS_EC2_KEY.pem #TODO change password
-scp -i AMOS_EC2_KEY.pem -o BatchMode=yes -o StrictHostKeyChecking=no ~/repo/backendRestDraft.tar.gz ubuntu@18.216.122.218:/home/ubuntu
-rm ~/repo/backendRestDraft.tar.gz
-ssh -i AMOS_EC2_KEY.pem -o BatchMode=yes -o StrictHostKeyChecking=no ubuntu@18.216.122.218 "sudo kill $(ps aux | grep 'java -jar pretrendr-' | awk '{print $2}' || true) || true; sudo rm -rf /home/ubuntu/pretrendr/backend/backendRestDraft || true; sudo rm -rf /home/ubuntu/pretrendr/backend/backendRestDraft || true; sudo mv /home/ubuntu/backendRestDraft.tar.gz /home/ubuntu/pretrendr/backend; sudo rm -rf /home/ubuntu/backendRestDraft.tar.gz; cd /home/ubuntu/pretrendr/backend; sudo tar -xvzf backendRestDraft.tar.gz; cd /home/ubuntu/pretrendr/backend/backendRestDraft; sudo sh run.sh"
+scp -i AMOS_EC2_KEY.pem -o BatchMode=yes -o StrictHostKeyChecking=no backendRestDraft.tar.gz ubuntu@18.216.122.218:/home/ubuntu
+rm backendRestDraft.tar.gz
+ssh -i AMOS_EC2_KEY.pem -o BatchMode=yes -o StrictHostKeyChecking=no ubuntu@18.216.122.218 "sudo kill $(ps aux | grep '/usr/lib/jvm/java-8-oracle/bin/java -classpath /usr/share/maven' | awk '{print $2}' || true) || true; sudo rm -rf /home/ubuntu/pretrendr/backend/repo || true; sudo mv /home/ubuntu/backendRestDraft.tar.gz /home/ubuntu/pretrendr/backend; sudo rm -rf /home/ubuntu/backendRestDraft.tar.gz; cd /home/ubuntu/pretrendr/backend; sudo tar -xvzf backendRestDraft.tar.gz; cd /home/ubuntu/pretrendr/backend/repo; sudo rm /home/ubuntu/pretrendr/backend/backendRestDraft.tar.gz; cd /home/ubuntu/pretrendr/backend/repo/backendRestDraft; sudo sh run.sh </dev/null &>/dev/null &"
 sudo rm AMOS_EC2_KEY.pem
