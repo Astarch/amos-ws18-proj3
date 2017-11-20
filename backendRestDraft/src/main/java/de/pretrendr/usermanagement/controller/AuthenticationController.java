7daeff8435957516e584ce29a53019c7f6588c2e
Tristan Schneider <schneidtha@gmail.com>
2017-11-20 01:01:20 +0100

    git repo integration

--- backendRestDraft/src/main/java/de/pretrendr/usermanagement/controller/AuthenticationController.java
+++ backendRestDraft/src/main/java/de/pretrendr/usermanagement/controller/AuthenticationController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import de.pretrendr.boot.git.GitRepositoryState;
 import de.pretrendr.usermanagement.businesslogic.UserService;
 import de.pretrendr.usermanagement.dataccess.UserDAO;
 import de.pretrendr.usermanagement.model.User;
@@ -24,6 +25,7 @@ import de.pretrendr.usermanagement.model.pojo.RegUser;
 public class AuthenticationController {
 	private final UserService userService;
 	private final UserDAO userDAO;
+	private GitRepositoryState gitRepositoryState;
 
 	/**
 	 * Autowired constructor.
@@ -33,9 +35,11 @@ public class AuthenticationController {
 	 * @author Tristan Schneider
 	 */
 	@Autowired
-	public AuthenticationController(final UserService userService, final UserDAO userDAO) {
+	public AuthenticationController(final UserService userService, final UserDAO userDAO,
+			GitRepositoryState gitRepositoryState) {
 		this.userService = userService;
 		this.userDAO = userDAO;
+		this.gitRepositoryState = gitRepositoryState;
 	}
 
 	/**
@@ -52,7 +56,17 @@ public class AuthenticationController {
 		} else {
 			return new ResponseEntity<>(HttpStatus.FOUND);
 		}
+	}
 
+	/**
+	 * @param username
+	 * @param password
+	 * @return
+	 * @author Tristan Schneider
+	 */
+	@RequestMapping(value = "/version", method = RequestMethod.GET)
+	public ResponseEntity<GitRepositoryState> version() {
+		return new ResponseEntity<GitRepositoryState>(gitRepositoryState, HttpStatus.OK);
 	}
 
 }
