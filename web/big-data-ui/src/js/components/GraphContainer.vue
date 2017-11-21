be9d4652a8f7b47bdfebb27d4e2914e0432cfa91
lara-c <christmann@campus.tu-berlin.de>
2017-11-21 13:03:51 +0100

    Fallback if backend not availiable to static dummy data

--- web/big-data-ui/src/js/components/GraphContainer.vue
+++ web/big-data-ui/src/js/components/GraphContainer.vue
@@ -31,27 +31,29 @@
     mounted: function () {
       var self = this;
 
-      // read external csv | !!!keep for testing!!!
-
-      /**
-      d3.csv('/static/data/test.csv', function (err, csvdata) {
-        self.dataset = csvdata.slice(0);
-        csvdata.forEach(function (d) {
-          // here, each value which should be numeric must be converted from string
-          d.count = +d.count;
-        });
-      }); **/
-
       // http GET
       this.$http.get('http://localhost:8081/api/dummy/graphData').then(response => {
         // status: ok
         if(response.status ==200){
           console.log("status: " + response.status);
           self.dataset = response.body;
+        } else {
+          self.readCSV();
         }
       }, response => {
         console.log("error: " + response.status);
+        self.readJSON();
+      });
+    },
+    // read external json | !!!keep for testing and fallback!!!
+    methods: {
+      readJSON: function () {
+      var self = this;
+      d3.json('/static/data/test.json', function (err, data) {
+        self.dataset = data;
+        console.log(self.dataset);
       });
     }
+  }
   };
 </script>
