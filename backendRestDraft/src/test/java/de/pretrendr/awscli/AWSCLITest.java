fc429fc017dae20920fe523a4577546de4ba31b7
Florian <florian.muchow@online.de>
2017-12-07 19:05:10 +0100

    test aws cli for circleci

--- backendRestDraft/src/test/java/de/pretrendr/awscli/AWSCLITest.java
+++ backendRestDraft/src/test/java/de/pretrendr/awscli/AWSCLITest.java
@@ -8,13 +8,14 @@ import org.junit.Test;
 import org.springframework.context.annotation.Bean;
 
 import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 
 public class AWSCLITest{
 
 	@Test
 	public void checkAWSCLIConnection() throws Exception{
-		ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
-		assertTrue("Bad AWS CLI credentials",(credentialsProvider.getCredentials() != null));
+		//ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
+		assertTrue("Bad AWS CLI credentials",(new EnvironmentVariableCredentialsProvider().getCredentials() != null));
 	}
 }
