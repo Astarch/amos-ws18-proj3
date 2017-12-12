17bf48642860c5000887e1222dcff734908e4ec3
Patrick Willmann <patrick.m.willmann@gmail.com>
2017-12-12 17:00:47 +0100

    started to add new landing page

--- web/big-data-ui/src/js/components/engagement/EngagementLayout.vue
+++ web/big-data-ui/src/js/components/engagement/EngagementLayout.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="wrapper">
+  <div id="engagement-layout">
+    <div class="wrapper">
     <side-bar type="sidebar" :sidebar-links="$sidebar.sidebarLinks">
 
     </side-bar>
@@ -12,6 +13,35 @@
 
       <content-footer></content-footer>
     </div>
+    </div>
+
+    <!--This sidebar appears only for screens smaller than 992px-->
+    <side-bar type="navbar" :sidebar-links="$sidebar.sidebarLinks">
+      <ul class="nav navbar-nav">
+        <li>
+          <a class="dropdown-toggle" data-toggle="dropdown">
+            <i class="ti-panel"></i>
+            <p>Stats</p>
+          </a>
+        </li>
+        <drop-down title="5 Notifications" icon="ti-bell">
+
+          <li><a>Notification 1</a></li>
+          <li><a>Notification 2</a></li>
+          <li><a>Notification 3</a></li>
+          <li><a>Notification 4</a></li>
+          <li><a>Another notification</a></li>
+
+        </drop-down>
+        <li>
+          <a>
+            <i class="ti-settings"></i>
+            <p>Settings</p>
+          </a>
+        </li>
+        <li class="divider"></li>
+      </ul>
+    </side-bar>
   </div>
 </template>
 <script>
@@ -36,5 +66,5 @@
 
 </script>
 <style lang="scss">
-
+  @import '../../../scss/paper-dashboard';
 </style>
